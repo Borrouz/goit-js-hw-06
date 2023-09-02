f60186d2c4eaf963998467961e3f316b6b9ebef9
Borrouz <brovkov.ievgen@gmail.com>
2023-09-02 21:07:37 +0300

    ready task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+'use strict'
 
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', formSubmit);
+
+function formSubmit (e) {
+    e.preventDefault()
+    const formElements = e.currentTarget.elements;
+    const mail = e.currentTarget.elements.email.value;
+    const password = e.currentTarget.elements.password.value;
+    if (mail === '' || password === '') {
+        alert('Please fill in all the fields!');
+        return
+    } else {
+        const formData = {mail, password}
+        console.log(formData);
+        form.reset()
+    }
+}
