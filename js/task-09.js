832ee9c4d9535396d54bdd7f556601e79d85085c
Borrouz <brovkov.ievgen@gmail.com>
2023-09-02 21:30:35 +0300

    ready test-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+'use strict'
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+
+const changeButton = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+changeButton.addEventListener('click', onClickButton);
+
+function onClickButton() {
+  body.style.backgroundColor = spanColor.textContent = getRandomHexColor();
 }
